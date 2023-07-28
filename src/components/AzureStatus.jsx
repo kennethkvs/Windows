7d6ad264938cb33b04c2efb8175c78b7662a30e1
Andrew Sivabalan <andrew.sivabalan@npxinnovation.ca>
2023-07-28 11:31:06 -0400

    added azurestatus box

---
+++ src/components/AzureStatus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { WindowsLogo } from "@phosphor-icons/react";
+import { ArrowElbowDownRight } from "@phosphor-icons/react";
+
+export const AzureStatus = (url) => {
+  return (
+    <div className="box-border h-64 w-64 border-4 p-4">
+      <WindowsLogo size={32} />
+    </div>
+  );
+};
